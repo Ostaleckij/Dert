960d9e2b83c31868e0f7b6a47bb4230f4606dc8f
Ostaleckij <91618902+Ostaleckij@users.noreply.github.com>
2021-11-26 14:49:17 +0200

    Add files via upload

---
+++ Lesson 15/AddTaskForm.js
@@ -0,0 +1,33 @@
+export default class AddTaskForm {
+    constructor( onCreateTask ) {
+        this.rootEl = document.querySelector('.header');
+        this.completeEl = this.rootEl.querySelector('.complete-all');
+        this.taskEl = this.rootEl.elements.task;
+        this.onCreateTask = onCreateTask;
+
+        this.rootEl.addEventListener('submit', this.createTask.bind(this));
+    }
+
+    createTask(e) {
+        e.preventDefault();
+
+        const task = {
+            completed: this.completeEl.checked,
+            text: this.taskEl.value
+        };
+
+        this.taskEl.value = '';
+
+        if (this.onCreateTask) {
+            this.onCreateTask(task);
+        }
+    }
+
+    setDisabled( disabled ) {
+        if ( disabled ) {
+            this.rootEl.classList.add('disabled');
+        } else {
+            this.rootEl.classList.remove('disabled');
+        }
+    }
+}
